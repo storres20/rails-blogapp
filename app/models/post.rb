c563ae789c8011788bb9894910766f55e79920cf
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-24 11:40:08 -0500

    Add call back for increment methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, class_name: "Comment", foreign_key: "post_id"
   has_many :likes, class_name: "Like", foreign_key: "post_id"
   
+  after_save :update_posts_counter
+
+  private
+  
   def update_posts_counter
     author.increment!(:posts_counter)
   end
