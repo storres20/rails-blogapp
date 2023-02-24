7531df99707a659dfcd6d4dad34127bff585b6f0
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-24 13:00:46 -0500

    Fix data error

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_044021) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id"
     t.bigint "post_id"
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
