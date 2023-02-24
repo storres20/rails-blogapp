3093a187885f75f5480d0f004e87a91f5d215891
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-23 23:41:01 -0500

    Add user ref to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_043925) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_044021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,7 +41,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_043925) do
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,5 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_043925) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
