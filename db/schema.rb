d7ac93380f269bcce54b65e79c5d58b8c06ee178
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-23 23:21:28 -0500

    Add post ref to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_031803) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_041551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_031803) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -30,8 +32,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_031803) do
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
+    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,6 +45,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_031803) do
     t.string "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users", column: "author_id"
 end
