c0cdd75da38c598df1ed0c26dd3e3b9777b71d97
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-24 11:44:32 -0500

    Fix rubocop linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: "author_id"
-  has_many :comments, class_name: "Comment", foreign_key: "post_id"
-  has_many :likes, class_name: "Like", foreign_key: "post_id"
-  
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+
   after_save :update_posts_counter
 
   private
-  
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
