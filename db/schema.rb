a7ff53ff959e1a110dc5db3c6032a1cf5adaa9a7
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-23 22:13:14 -0500

    Create Post table and db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_015823) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_031210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
