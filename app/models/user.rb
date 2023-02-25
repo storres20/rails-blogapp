6369adcf833c8c6ddbf19663d60f46b21ca752c4
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-24 23:09:43 -0500

    Fix rubocop linter's error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :comments, class_name: "Comment", foreign_key: "author_id"
-  has_many :likes, class_name: "Like", foreign_key: "author_id"
-  has_many :posts, class_name: "Post", foreign_key: "author_id"
+  has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'author_id'
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
 
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
