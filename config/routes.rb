60f89587fed7b1aaa59e9f09d3a100fff18b42ea
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-04 13:36:34 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 
   root "users#index"
